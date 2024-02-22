b1b8fd697c8039e4aa56c5435333014555a9ac32
Eduardo Tavares <eduardo_tvn@hotmail.com>
2024-02-22 19:04:10 -0300

    Revert "Adicionado Relatório"
    This reverts commit d331ded2222ca5d6f9afdfbfbbd93e5511b79c74.

--- src/countAdjacentBombs.asm
+++ src/countAdjacentBombs.asm
@@ -14,23 +14,23 @@ countAdjacentBombs:
 
  loop_i:
   	bge $t0, 2, end_loop_i					#se t0 == 2, finaliza o programa
- 	add $t2, $s2, $t0 # i = linha + contador t0 
+ 	add $t2, $s2, $t0 # i = linha + contador t0		#t2 = 5 + t0, primeira rodada = 4, segunda = 5, terceira = 6 
  	blt $t2, $zero, next_i_step				#se t2 < 0, proxima parte do loop
  	bge $t2, SIZE, end_loop_i				# se t2 == 8, finaliza o loop (nao tem mais linhas)
  	
  	li $t1, -1 #iniciando contador j 
  	loop_j:
  	 	bge $t1, 2, end_loop_j				#se t1 == 2, finaliza loop j (proximo loop i)
- 		add $t3, $s3, $t1 # j = coluna + contador t1	5
+ 		add $t3, $s3, $t1 # j = coluna + contador t1	#t3 = 3 + t1, primeira rodada = 3 , segunda = 4, terceira = 5
  		blt $t3, $zero, next_j_step			#se t3 < 0, proxima parte do loop
  		bge $t3, SIZE, end_loop_j			#se t3 == 8, finaliza o loop j (nao tem mais colunas)
  		
  		#Calculate the board position
- 		mul $t4, $t2, SIZE				
- 		add $t4, $t4, $t3				
- 		sll $t4, $t4, 2					
- 		add $t5, $t4, $s0				
- 		lw $t6, 0($t5)					
+ 		mul $t4, $t2, SIZE				#adiciona ao registrador t4 o valor 4*8 = 32
+ 		add $t4, $t4, $t3				#adiciona ao registrador t4 o valor t4 + t3 = 32 + 3 = 35
+ 		sll $t4, $t4, 2					#multiplica o valor no registrador t4 por 4 = 35*4 = 140
+ 		add $t5, $t4, $s0				#adiciona ao registrador t5 o endereço t4 + s0 ~= 140
+ 		lw $t6, 0($t5)					#adiciona ao registrador t6 o conteudo de 72 ''supondo -1''
  		
  		bne $t6, -1, not_bomb				#é igual a -1
  		
